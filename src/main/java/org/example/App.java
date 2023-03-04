ade2bec2626504898c4f0e51c90f779128cae6eb
Genie16-github <4411dlwoqja@naver.com>
2023-03-05 08:51:41 +0900

    5단계 구현완료

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -12,22 +14,33 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        long lastWiseSayingId = 1;
+        long lastWiseSayingId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
+        label:
         while (true) {
             System.out.print("명령) ");
             String command = sc.nextLine().trim();   // 좌우 공백 제거
-            if (command.equals("종료")) {
-                break;
-            } else if (command.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = sc.nextLine().trim();
-                System.out.print("작가 : ");
-                String authorName = sc.nextLine().trim();
+            switch (command) {
+                case "종료":
+                    break label;
+                case "등록":
+                    long id = lastWiseSayingId + 1;
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.print("작가 : ");
+                    String authorName = sc.nextLine().trim();
 
-                System.out.println("1번 명언이 등록되었습니다.");
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId);
-                lastWiseSayingId++;                    // 인덱스 증가
+                    WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
+                    wiseSayings.add(wiseSaying);
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    lastWiseSayingId = id;                // 인덱스 증가
+
+                    break;
+                case "목록":
+                    System.out.printf("생성된 명언 수 : %d\n", wiseSayings.size());
+                    break;
             }
         }
     }
