f9f66bafe34efac0f3ef81935f769745b5abab26
Genie16-github <4411dlwoqja@naver.com>
2023-03-05 09:24:55 +0900

    8단계 구현완료

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -28,6 +28,9 @@ public class App {
                 case "삭제":
                     wiseSayingController.remove(rq);
                     break;
+                case "수정":
+                    wiseSayingController.modify(rq);
+                    break;
             }
         }
     }
